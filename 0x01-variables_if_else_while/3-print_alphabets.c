d148860a37934d7281a7b47623afd742ba9cf161
dawit-silas <dawit.tes.003@gmail.com>
2022-09-09 05:47:01 -0700

    commit on solution #3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	char d;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	for (d = 'A'; d <= 'Z'; d++)
+	{
+		putchar(d);
+		if (d == 'Z')
+			putchar('\n');
+	}
+	return (0);
+}
