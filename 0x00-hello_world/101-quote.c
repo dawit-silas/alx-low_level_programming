fd276222b1d6828d7f201839ee8687a1ba115b71
dawit-silas <dawit.tes.003@gmail.com>
2022-09-08 17:43:31 -0700

    w

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
 int main(void)
 {
 	char g[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
