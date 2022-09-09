46bd8b6821c5c6ac1ff7b488c45df828ebac2fcc
dawit-silas <dawit.tes.003@gmail.com>
2022-09-08 17:22:32 -0700

    final commit on solution #8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	char g[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(g, 59, 1, stderr);
 	return (1);
 }
