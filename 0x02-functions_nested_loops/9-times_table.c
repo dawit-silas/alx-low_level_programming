3da0f61a2e462778d9e6f6c2570f03e0f8260d4a
dawit-silas <dawit.tes.003@gmail.com>
2022-09-13 14:40:42 -0700

    2nd commit on solution #9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,19 +19,19 @@ void times_table(void)
 			m = i * j;
 			if (m < 10)
 			{
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(m + '0');
 			}
 			else
 			{
+				_putchar(' ');
 				_putchar(m / 10 + '0');
 				_putchar(m % 10 + '0');
 			}
 			if (j != 9)
 			{
 				_putchar(',');
-				_putchar(' ');
-				if (m < 10)
-					_putchar(' ');
 			}
 		}
 		_putchar('\n');
