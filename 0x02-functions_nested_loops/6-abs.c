7a62624e276e482b13db8c7b3fecfd62ace96fc8
dawit-silas <dawit.tes.003@gmail.com>
2022-09-13 10:41:38 -0700

    commit on solution #6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+/**
+ * int _abs - return abs of a number
+ *
+ * Return: return positive nums
+ *
+ * @n: param
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+}
