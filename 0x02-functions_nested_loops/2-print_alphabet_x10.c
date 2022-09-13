daf4b9575bf8bde53ec72ca46dfdfbb8a2be464a
dawit-silas <dawit.tes.003@gmail.com>
2022-09-13 08:58:32 -0700

    commit on solution #2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints a to z 10x
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
