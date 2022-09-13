3986590cf218512ea463dd010c3c41c51ffd2fef
dawit-silas <dawit.tes.003@gmail.com>
2022-09-13 14:19:45 -0700

    Comment on solution #9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - time table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int m;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			m = i * j;
+			if (m < 10)
+			{
+				_putchar(m + '0');
+			}
+			else
+			{
+				_putchar(m / 10 + '0');
+				_putchar(m % 10 + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (m < 10)
+					_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
