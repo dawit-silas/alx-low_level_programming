435bf44133644489281e1148e1dc14a0e949f614
dawit-silas <dawit.tes.003@gmail.com>
2022-09-13 17:03:53 -0700

    3rd commit on solution 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,12 +8,42 @@
  * @n: param
  */
 
+void printItems(int i, int j, int n)
+{
+	int m = i * j;
+
+	if (m < 10)
+	{
+		if (j != 0)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar(m + '0');
+	}
+	else if (m < 100 && m >= 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(m / 10 + '0');
+		_putchar(m % 10 + '0');
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar(m / 100 + '0');
+		_putchar((m % 100) / 10 + '0');
+		_putchar((m % 100) % 10 + '0');
+	}
+	if (j != n)
+		_putchar(',');
+}
+
 void print_times_table(int n)
 {
-
 	int i;
 	int j;
-	int m;
 
 	if (n < 0 || n > 15)
 		return;
@@ -22,33 +52,7 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			m = i * j;
-			if (m < 10)
-			{
-				if (j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				_putchar(m + '0');
-			}
-			else if (m < 100 && m >= 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(m / 10 + '0');
-				_putchar(m % 10 + '0');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(m / 100 + '0');
-				_putchar((m % 100) / 10 + '0');
-				_putchar((m % 100) % 10 + '0');
-			}
-			if (j != n)
-				_putchar(',');
+			printItems(i, j, n);
 		}
 		_putchar('\n');
 	}
