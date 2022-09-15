d91fb7fd01c4021e5f09104a0c6be9ea2dfedbcc
dawit-silas <dawit.tes.003@gmail.com>
2022-09-15 15:11:07 -0700

    commiyt

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,6 @@
 void positive_or_negative(int);
 int largest_number(int, int, int);
 int convert_day(int, int);
-void print_remaining_days(int, int, int);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
