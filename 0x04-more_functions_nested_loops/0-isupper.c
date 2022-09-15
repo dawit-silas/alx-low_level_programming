09eaafb221bbfd04e9c5ae57ea724e3e900c9e54
dawit-silas <dawit.tes.003@gmail.com>
2022-09-15 11:57:31 -0700

    commit on task #0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+/**
+ * _isupper - method rhat check if uppercase
+ *
+ * @c: prama
+ *
+ * Return: int
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
