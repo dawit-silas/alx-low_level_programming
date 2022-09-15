d6e0cdecd7439a6d67d5b0a810d478511779a8a9
dawit-silas <dawit.tes.003@gmail.com>
2022-09-15 10:51:21 -0700

    main edited

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
+void positive_or_negative(int);
 int largest_number(int, int, int);
 int convert_day(int, int);
 void print_remaining_days(int, int, int);
