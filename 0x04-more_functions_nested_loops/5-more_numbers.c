b49b136944deeab2db53b7e1ddb8307f70a7b958
dawit-silas <dawit.tes.003@gmail.com>
2022-09-15 13:09:32 -0700

    commit on task #5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - function to print 1 to 14
+ * ten times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
