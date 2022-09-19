94191942de0445917a06be421f2075622fdc8c83
dawit-silas <dawit.tes.003@gmail.com>
2022-09-19 04:13:57 -0700

    commit on task #4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - write strings in reverse manner
+ *
+ * @s: param string
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	i = len - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
