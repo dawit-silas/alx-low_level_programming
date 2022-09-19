ec4106a181fc7fbb8485bde5e856ff1d3beeb4e6
dawit-silas <dawit.tes.003@gmail.com>
2022-09-19 15:00:15 -0700

    final commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,12 +7,15 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
+
+	dest[i] = '\0';
 
 	return (dest);
 }
