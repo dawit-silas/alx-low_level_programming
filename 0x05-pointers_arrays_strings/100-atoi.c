1069126af50d4dea7c41968b0edb55b4575bea73
dawit-silas <dawit.tes.003@gmail.com>
2022-09-19 16:38:40 -0700

    2nd commit on task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,25 +7,24 @@
 
 int _atoi(char *s)
 {
-	int i = 0, num = 0, j = 0;
+	int i = 0, num = 0, sign = 1;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			j = i;
 			while (s[i] >= '0' && s[i] <= '9')
 			{
 				num *= 10;
 				num += s[i] - '0';
 				i++;
 			}
-			if (j > 0)
-			{
-				if (s[j - 1] == '-')
-					num *= -1;
-			}
+			num *= sign;
 			break;
+		}
+		if (s[i] == '-')
+		{
+			sign *= -1;
 		}
 		i++;
 	}
