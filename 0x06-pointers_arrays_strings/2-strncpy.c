d1db5372b13ff6992bdff1b1671f654fafbd2bc2
dawit-silas <dawit.tes.003@gmail.com>
2022-09-21 09:30:56 -0700

    commit on task #2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+/**
+ * _strncpy - copy a string to nth index
+ * @dest: param1
+ * @src: param2
+ * @src: param3
+ * Return: char*
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	while (i < n)
+	{
+		if (i < len)
+		{
+			dest[i] = src[i];
+		}
+		else
+		{
+			dest[i] = '\0';
+		}
+		i++;
+	}
+
+	for (i = n; dest[i] != '\0'; i++)
+	{
+		dest[i] = '*';
+	}
+	dest[i] = '\0';
+	return (dest);
+}
