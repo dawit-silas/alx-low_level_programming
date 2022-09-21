1de4507e63adfed198a322e58a9c267288fbe0a9
dawit-silas <dawit.tes.003@gmail.com>
2022-09-21 10:30:22 -0700

    commit on task #3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+/**
+ * _strcmp - function to campare
+ * @s1: param1
+ * @s2: param2
+ * Return: str
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (s2 < s1)
+	{
+		return (15);
+	}
+	else if (s2 > s1)
+	{
+		return (-15);
+	}
+	return (0);
+}
