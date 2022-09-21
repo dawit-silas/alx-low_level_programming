fdef8a4848efc37b73d809b928847ba44be873e8
dawit-silas <dawit.tes.003@gmail.com>
2022-09-21 11:53:53 -0700

    commit on task #5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+/**
+ * string_toupper - fun to upper case
+ * @s: param
+ * Return: char*
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
+}
