bc8b34190dd5f027cd0eb9cbf1c66e869d248ea6
dawit-silas <dawit.tes.003@gmail.com>
2022-09-21 10:53:42 -0700

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s2 < s1)
+	while (*s2 && *s1 && *s2 == *s1)
 	{
-		return (15);
+		s2++;
+		s1++;
 	}
-	else if (s2 > s1)
-	{
-		return (-15);
-	}
-	return (0);
+	return (*s2 - *s1)
 }
