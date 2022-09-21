66036c887f5d8214dc3c15267786b71ee48d7cad
dawit-silas <dawit.tes.003@gmail.com>
2022-09-21 11:14:55 -0700

    dddd

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,10 +7,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s2 && *s1 && *s2 == *s1)
+	while (*s1 != '\0' && *s2 != '\0' && *s2 == *s1)
 	{
 		s2++;
 		s1++;
 	}
-	return (*s2 - *s1)
+	return (*s1 - *s2);
 }
