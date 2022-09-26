0a8b1aabab6172554ba82efebe8b6f47d4dca202
dawit-silas <dawit.tes.003@gmail.com>
2022-09-26 07:14:30 -0700

    commit on task #2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strchr - first matching character
+ * @s: param
+ * @c: param
+ * Return: chr * 
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s && (char)*s != c)
+		++s;
+	return (char *)(c == (char)*s ? s : NULL);
+}
