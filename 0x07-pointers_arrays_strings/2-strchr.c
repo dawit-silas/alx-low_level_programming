364d826d687c26c5f7d1c9f323f161bcabe2d0e9
dawit-silas <dawit.tes.003@gmail.com>
2022-09-26 07:19:05 -0700

    commit on task #2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,12 +4,12 @@
  * _strchr - first matching character
  * @s: param
  * @c: param
- * Return: chr * 
+ * Return: chr *
  */
 
 char *_strchr(char *s, char c)
 {
 	while (*s && (char)*s != c)
 		++s;
-	return (char *)(c == (char)*s ? s : NULL);
+	return ((char *)(c == (char)*s ? s : NULL));
 }
