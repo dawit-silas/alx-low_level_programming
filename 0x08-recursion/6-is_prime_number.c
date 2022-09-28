691b9cf2ce623b1ec8f8498ad1bc882fd40eb706
dawit-silas <dawit.tes.003@gmail.com>
2022-09-28 11:59:28 -0700

    hhhhh

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+/**
+ * check_prime - fun check
+ * @n: target num
+ * @i: iterator
+ * Return: int
+ */
+
+int check_prime(int n, int i)
+{
+	if(i == 1)
+	{
+		return (0);
+	}
+	else
+	{
+		if (n % i == 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (check_prime(n, i - 1));
+		}
+	}
+}
+
+/**
+ * is_prime_number - func
+ * @n: param
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	return (check_prime(n, n / 2));
+}
