918669f4f114fa368076138fe68f9fbbd1c7085f
dawit-silas <dawit.tes.003@gmail.com>
2022-09-28 10:31:25 -0700

    commit on task #4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _pow_recursion - func
+ * @x: param
+ * @y: param
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y-1));
+}
