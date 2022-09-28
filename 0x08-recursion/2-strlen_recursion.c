7ba9dd82a048342e05af0c0d78f97c70ff0fc250
dawit-silas <dawit.tes.003@gmail.com>
2022-09-28 09:50:27 -0700

    commit on task #2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen_recursion - func
+ * @s: param
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
